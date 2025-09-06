b8fb9e9e7ca5cb44f532e85039f64e006f2cd7a8
yousef <youssef.ahmmed29@gmail.com>
2025-09-06 15:29:45 +0300

    Implement common pagination dto

---
+++ src/common/dto/pagination.dto.ts
@@ -0,0 +1,43 @@
+import { Type } from "class-transformer";
+import { IsInt, IsOptional, Max, Min, IsString } from "class-validator";
+
+export class PaginationDto {
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page: number = 1;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(100)
+  limit: number = 20;
+
+  @IsOptional()
+  @IsString()
+  search?: string;
+
+  @IsOptional()
+  @IsString()
+  role?: string;
+}
+
+export class PaginationMetaDto {
+  page: number;
+  limit: number;
+  total: number;
+  pages: number;
+  hasNext: boolean;
+  hasPrevious: boolean;
+
+  constructor(page: number, limit: number, total: number) {
+    this.page = page;
+    this.limit = limit;
+    this.total = total;
+    this.pages = Math.ceil(total / limit);
+    this.hasNext = page < this.pages;
+    this.hasPrevious = page > 1;
+  }
+}
